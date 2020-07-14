75407520d3289c9f8560449643376e04aae4522b
Rick Poyner <rick.poyner@tri.global>
2020-07-14 13:47:11 -0400

    wip

--- multibody/plant/multibody_plant.cc
+++ multibody/plant/multibody_plant.cc
@@ -1320,12 +1320,19 @@ void MultibodyPlant<T>::CalcContactResultsContinuousPointPair(
             context.get_time(),
             fmt::format("context: {}\ncontinuous {}\n"
                         "contact_point {}\n"
-                        "contact A {}\ncontact B {}\n",
+                        "contact A {}\ncontact B {}\n"
+                        "gid A {}\ngid B {}\n"
+                        "bni A {}\nbni B {}\n",
                         context.to_string(),
                         tmp::vec_fmt(context.get_continuous_state_vector()),
                         tmp::vec_fmt(p_WC),
                         tmp::vec_fmt(p_WCa),
-                        tmp::vec_fmt(p_WCb)));
+                        tmp::vec_fmt(p_WCb),
+                        geometryA_id,
+                        geometryB_id,
+                        static_cast<int>(bodyA_node_index),
+                        static_cast<int>(bodyB_node_index)
+                        ));
     }
 
     // Contact point position on body A.
