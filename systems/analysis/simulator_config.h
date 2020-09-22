1d3ac2be22a247b99dac4538f6a18073f0f4e097
Rick Poyner <rick.poyner@tri.global>
2020-09-22 18:00:15 -0400

    wip

--- systems/analysis/simulator_config.h
+++ systems/analysis/simulator_config.h
@@ -12,12 +12,6 @@ namespace systems {
 struct SimulatorConfig {
   template <typename Archive>
   void Serialize(Archive* a) {
-    a->Visit(DRAKE_NVP(integration_scheme));
-    a->Visit(DRAKE_NVP(max_step_size));
-    a->Visit(DRAKE_NVP(accuracy));
-    a->Visit(DRAKE_NVP(use_error_control));
-    a->Visit(DRAKE_NVP(target_realtime_rate));
-    a->Visit(DRAKE_NVP(publish_every_time_step));
   }
 
   std::string integration_scheme{"runge_kutta3"};
