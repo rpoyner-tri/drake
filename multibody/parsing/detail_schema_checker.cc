0b27241477276e38cff75887a21b2ae937dc33a3
Rick Poyner <rick.poyner@tri.global>
2023-11-17 19:20:26 -0500

    wip: add/refactor for string or file input

--- multibody/parsing/detail_schema_checker.cc
+++ multibody/parsing/detail_schema_checker.cc
@@ -21,11 +21,22 @@ class ErrorHandler {
   ErrorHandler(
       const DiagnosticPolicy& diagnostic) : diagnostic_(diagnostic) {}
 
+  // Received a structured error from a libxml2 parser.
+  // @param userData   the "context" associated with this callback.
+  // @param error      the error description struct from the parser.
+  // @pre userData must be a pointer to an `ErrorHandler` instance.
+  //
+  // Note: this entry point is designed to match the `xmlStructuredErrorFunc`
+  // type required by libxml2. The precondition is not checked (nor is it
+  // checkable); mistakes will result in undefined behavior (most likely
+  // segfault).
   static void ReceiveStructuredError(void* userData, xmlErrorPtr error) {
+    DRAKE_DEMAND(userData != nullptr);
     ErrorHandler* handler = static_cast<ErrorHandler*>(userData);
     handler->Receive(error);
   }
 
+ private:
   void Receive(xmlErrorPtr error) {
     DRAKE_DEMAND(error != nullptr);
     switch (error->level) {
@@ -42,9 +53,9 @@ class ErrorHandler {
     }
   }
 
- private:
   DiagnosticDetail Convert(xmlErrorPtr error) {
     DRAKE_DEMAND(error != nullptr);
+    DRAKE_DEMAND(error->message != nullptr);
     DiagnosticDetail detail;
     detail.filename = std::string(error->file == nullptr ? "" : error->file);
     detail.line = error->line;
@@ -54,24 +65,21 @@ class ErrorHandler {
 
   const DiagnosticPolicy& diagnostic_;
 };
-}  // namespace
 
-int CheckDocumentAgainstRngSchema(
+
+int CheckDocumentAgainstRngParser(
     const DiagnosticPolicy& diagnostic,
-    const std::filesystem::path& rng_schema,
+    xmlRelaxNGParserCtxtPtr rngparser,
     const std::filesystem::path& document) {
+  DRAKE_DEMAND(rngparser != nullptr);
+
   ErrorHandler error_handler(diagnostic);
+
+  xmlRelaxNGSetParserStructuredErrors(
+      rngparser, ErrorHandler::ReceiveStructuredError, &error_handler);
 
   xmlDoc *doc = xmlParseFile(document.c_str());
   ScopeExit doc_guard([&doc]() { xmlFreeDoc(doc); });
-
-  xmlRelaxNGParserCtxtPtr rngparser =
-      xmlRelaxNGNewParserCtxt(rng_schema.c_str());
-  ScopeExit rngparser_guard([&rngparser]() {
-    xmlRelaxNGFreeParserCtxt(rngparser);
-  });
-  xmlRelaxNGSetParserStructuredErrors(
-      rngparser, ErrorHandler::ReceiveStructuredError, &error_handler);
 
   xmlRelaxNGPtr schema = xmlRelaxNGParse(rngparser);
   ScopeExit schema_guard([&schema]() { xmlRelaxNGFree(schema); });
@@ -86,6 +94,33 @@ int CheckDocumentAgainstRngSchema(
   int status = xmlRelaxNGValidateDoc(validctxt, doc);
 
   return status;
+}
+
+}  // namespace
+
+int CheckDocumentAgainstRngSchema(
+    const DiagnosticPolicy& diagnostic,
+    const std::filesystem::path& rng_schema,
+    const std::filesystem::path& document) {
+  xmlRelaxNGParserCtxtPtr rngparser =
+      xmlRelaxNGNewParserCtxt(rng_schema.c_str());
+  ScopeExit rngparser_guard([&rngparser]() {
+    xmlRelaxNGFreeParserCtxt(rngparser);
+  });
+  return CheckDocumentAgainstRngParser(diagnostic, rngparser, document);
+}
+
+int CheckDocumentAgainstRngSchemaAsString(
+    const DiagnosticPolicy& diagnostic,
+    const std::string& rng_schema_string,
+    const std::filesystem::path& document) {
+  xmlRelaxNGParserCtxtPtr rngparser =
+      xmlRelaxNGNewMemParserCtxt(rng_schema_string.c_str(),
+                                 rng_schema_string.size());
+  ScopeExit rngparser_guard([&rngparser]() {
+    xmlRelaxNGFreeParserCtxt(rngparser);
+  });
+  return CheckDocumentAgainstRngParser(diagnostic, rngparser, document);
 }
 
 }  // namespace internal
--- multibody/parsing/detail_schema_checker.h
+++ multibody/parsing/detail_schema_checker.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <string>
 
 #include "drake/common/diagnostic_policy.h"
 
@@ -11,6 +12,11 @@ namespace internal {
 int CheckDocumentAgainstRngSchema(
     const drake::internal::DiagnosticPolicy& diagnostic,
     const std::filesystem::path& rng_schema,
+    const std::filesystem::path& document);
+
+int CheckDocumentAgainstRncSchemaAsString(
+    const drake::internal::DiagnosticPolicy& diagnostic,
+    const std::string& rnc_schema_string,
     const std::filesystem::path& document);
 
 }  // namespace internal
