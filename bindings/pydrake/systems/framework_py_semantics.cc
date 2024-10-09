f817dac0fa4f4b465729a397454a2e4428e25fed
Rick Poyner <rick.poyner@tri.global>
2024-10-09 18:21:38 -0400

    wip: add include

--- bindings/pydrake/systems/framework_py_semantics.cc
+++ bindings/pydrake/systems/framework_py_semantics.cc
@@ -3,6 +3,7 @@
 #include "drake/bindings/pydrake/common/cpp_template_pybind.h"
 #include "drake/bindings/pydrake/common/default_scalars_pybind.h"
 #include "drake/bindings/pydrake/common/eigen_pybind.h"
+#include "drake/bindings/pydrake/common/ref_cycle_pybind.h"
 #include "drake/bindings/pydrake/common/type_safe_index_pybind.h"
 #include "drake/bindings/pydrake/common/wrap_pybind.h"
 #include "drake/bindings/pydrake/documentation_pybind.h"
