7f21350c8c910601b7dc0a0ee1ed9b4513b726e4
Rick Poyner <rick.poyner@tri.global>
2024-11-15 17:32:26 -0500

    wip

--- systems/framework/event_collection.cc
+++ systems/framework/event_collection.cc
@@ -1,15 +1,40 @@
 #include "drake/systems/framework/event_collection.h"
 
+#define DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_EVENTS(SomeType)	\
+  template SomeType<::drake::systems::PublishEvent<double>>;		\
+  template SomeType<::drake::systems::DiscreteUpdateEvent<double>>;	\
+  template SomeType<::drake::systems::UnrestrictedUpdateEvent<double>>;	\
+  template SomeType<::drake::systems::PublishEvent<::drake::AutoDiffXd>>; \
+  template SomeType<::drake::systems::DiscreteUpdateEvent<::drake::AutoDiffXd>>; \
+  template SomeType<::drake::systems::UnrestrictedUpdateEvent<::drake::AutoDiffXd>>; \
+  template SomeType<::drake::systems::PublishEvent<::drake::symbolic::Expression>>; \
+  template SomeType<::drake::systems::DiscreteUpdateEvent<::drake::symbolic::Expression>>; \
+  template SomeType<::drake::systems::UnrestrictedUpdateEvent<::drake::symbolic::Expression>>;
+
 namespace drake {
 namespace systems {
-EventCollection::~EventCollection() {}
-LeafEventCollection::~LeafEventCollection() {}
-DiagramEventCollection::~DiagramEventCollection() {}
-CompositeEventCollection::~CompositeEventCollection() {}
-LeafCompositeEventCollection::~LeafCompositeEventCollection() {}
-DiagramCompositeEventCollection::~DiagramCompositeEventCollection() {}
+template <typename Event>
+EventCollection<Event>::~EventCollection() {}
+template <typename Event>
+LeafEventCollection<Event>::~LeafEventCollection() {}
+template <typename Event>
+DiagramEventCollection<Event>::~DiagramEventCollection() {}
+  
+template <typename T>
+CompositeEventCollection<T>::~CompositeEventCollection() {}
+template <typename T>
+LeafCompositeEventCollection<T>::~LeafCompositeEventCollection() {}
+  template <typename T>  
+DiagramCompositeEventCollection<T>::~DiagramCompositeEventCollection() {}
 }  // namespace systems
 }  // namespace drake
+
+DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_EVENTS(
+    class ::drake::systems::EventCollection);
+DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_EVENTS(
+    class ::drake::systems::LeafEventCollection);
+DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_EVENTS(
+    class ::drake::systems::DiagramEventCollection);
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::CompositeEventCollection);
