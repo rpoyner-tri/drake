b08fbe9f570e1fa045d095edd363750ecfb6200a
Rick Poyner <rick.poyner@tri.global>
2026-03-04 16:29:24 -0500

    wip

--- bindings/pydrake/pydrake_pybind.h
+++ bindings/pydrake/pydrake_pybind.h
@@ -292,15 +292,15 @@ std::shared_ptr<T> make_shared_ptr_from_py_object(py::object py_object) {
 #define DRAKE_NB_NUMPY_OBJECT_DTYPE(Type)       \
    NB_NUMPY_OBJECT_DTYPE(Type)
 */
-#define DRAKE_NB_NUMPY_OBJECT_DTYPE(Type)           \
-  namespace nanobind::detail {                      \
-  template <>                                       \
-  struct dtype_traits<Type> {                       \
-    static constexpr dlpack::dtype value{           \
-        dlpack::dtype::OpaqueHandle,                \
-        sizeof(object) * 8,                         \
-        1,                                          \
-    };                                              \
-    static constexpr auto name = const_name(#Type); \
-  };                                                \
+#define DRAKE_NB_NUMPY_OBJECT_DTYPE(Type)                       \
+  namespace nanobind::detail {                                  \
+  template <>                                                   \
+  struct dtype_traits<Type> {                                   \
+    static constexpr dlpack::dtype value{                       \
+        static_cast<uint8_t>(dlpack::dtype_code::OpaqueHandle), \
+        sizeof(object) * 8,                                     \
+        1,                                                      \
+    };                                                          \
+    static constexpr auto name = const_name(#Type);             \
+  };                                                            \
   }  // namespace nanobind::detail
